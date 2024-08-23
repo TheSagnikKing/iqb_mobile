773ba48135a6405ff39060dcf6b2cdc9bfae61e9
Sumit <meathdev@outlook.com>
2024-08-23 16:05:25 +0530

    commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,310 +1,310 @@
-// import React, { useEffect } from 'react';
-// import { Stack } from 'expo-router';
-// import { useFonts } from 'expo-font';
-// import * as SplashScreen from 'expo-splash-screen';
-// import { Provider } from 'react-redux';
-// import store from '../redux/store';
+import React, { useEffect } from 'react';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
-// const _layout = () => {
-//   const [loaded] = useFonts({
-//     'montserrat': require('./../assets/fonts/Montserrat-Regular.ttf'),
-//     'montserrat-medium': require('./../assets/fonts/Montserrat-Medium.ttf'),
-//     'montserrat-semibold': require('./../assets/fonts/Montserrat-SemiBold.ttf'),
-//     'montserrat-bold': require('./../assets/fonts/Montserrat-Bold.ttf'),
-//   });
+const _layout = () => {
+  const [loaded] = useFonts({
+    'montserrat': require('./../assets/fonts/Montserrat-Regular.ttf'),
+    'montserrat-medium': require('./../assets/fonts/Montserrat-Medium.ttf'),
+    'montserrat-semibold': require('./../assets/fonts/Montserrat-SemiBold.ttf'),
+    'montserrat-bold': require('./../assets/fonts/Montserrat-Bold.ttf'),
+  });
 
-//   useEffect(() => {
-//     if (loaded) {
-//       SplashScreen.hideAsync();
-//     }
-//   }, [loaded]);
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
 
-//   if (!loaded) {
-//     return null;
-//   }
+  if (!loaded) {
+    return null;
+  }
 
-//   return (
-//     <Provider store={store}>
-//       <Stack>
-//         <Stack.Screen
-//           name="index"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="signin"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="signup"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen name="forgot" />
-//         <Stack.Screen
-//           name="agree"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="activationcode"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="(tabs)"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="selectbarber"
-//           options={{
-//             headerTitle: 'Barber List',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="selectservices"
-//           options={{
-//             headerTitle: 'Select Service',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="autojoin"
-//           options={{
-//             headerTitle: 'Auto Join',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="joingroup"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="joingroupbarbers"
-//           options={{
-//             headerTitle: 'Select Barber',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="joingroupservices"
-//           options={{
-//             headerTitle: 'Select Services',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="saloninfo"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="salonbarberservices"
-//           options={{
-//             headerTitle: '',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="saloninfogallery"
-//           options={{
-//             headerTitle: 'Salon Gallery',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="changelocation"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="locationsalonlist"
-//           options={{
-//             headerTitle: 'Select Salon',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="connectsalon"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="connectsalonbarberservices"
-//           options={{
-//             headerTitle: '',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="conectsalongallery"
-//           options={{
-//             headerTitle: 'Salon Gallery',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="help"
-//           options={{
-//             headerTitle: 'Help',
-//           }}
-//         />
-//         <Stack.Screen
-//           name="profile"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//         <Stack.Screen
-//           name="editprofile"
-//           options={{
-//             headerShown: false,
-//           }}
-//         />
-//       </Stack>
-//     </Provider>
-//   );
-// };
+  return (
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="signin"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="signup"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="forgot" />
+        <Stack.Screen
+          name="agree"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="activationcode"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="selectbarber"
+          options={{
+            headerTitle: 'Barber List',
+          }}
+        />
+        <Stack.Screen
+          name="selectservices"
+          options={{
+            headerTitle: 'Select Service',
+          }}
+        />
+        <Stack.Screen
+          name="autojoin"
+          options={{
+            headerTitle: 'Auto Join',
+          }}
+        />
+        <Stack.Screen
+          name="joingroup"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="joingroupbarbers"
+          options={{
+            headerTitle: 'Select Barber',
+          }}
+        />
+        <Stack.Screen
+          name="joingroupservices"
+          options={{
+            headerTitle: 'Select Services',
+          }}
+        />
+        <Stack.Screen
+          name="saloninfo"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="salonbarberservices"
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name="saloninfogallery"
+          options={{
+            headerTitle: 'Salon Gallery',
+          }}
+        />
+        <Stack.Screen
+          name="changelocation"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="locationsalonlist"
+          options={{
+            headerTitle: 'Select Salon',
+          }}
+        />
+        <Stack.Screen
+          name="connectsalon"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="connectsalonbarberservices"
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name="conectsalongallery"
+          options={{
+            headerTitle: 'Salon Gallery',
+          }}
+        />
+        <Stack.Screen
+          name="help"
+          options={{
+            headerTitle: 'Help',
+          }}
+        />
+        <Stack.Screen
+          name="profile"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="editprofile"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </Provider>
+  );
+};
 
-// export default _layout;
+export default _layout;
 
 
 /// NOTIFICATION EXAMPLE
 
-import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
+// import { useState, useEffect, useRef } from 'react';
+// import { Text, View, Button, Platform } from 'react-native';
+// import * as Device from 'expo-device';
+// import * as Notifications from 'expo-notifications';
+// import Constants from 'expo-constants';
 
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: false,
+//     shouldSetBadge: false,
+//   }),
+// });
 
 
 
-async function sendPushNotification(expoPushToken) {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Original Title',
-    body: 'And here is the body!',
-    data: { someData: 'goes here' },
-  };
+// async function sendPushNotification(expoPushToken) {
+//   const message = {
+//     to: expoPushToken,
+//     sound: 'default',
+//     title: 'Original Title',
+//     body: 'And here is the body!',
+//     data: { someData: 'goes here' },
+//   };
 
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
-}
+//   await fetch('https://exp.host/--/api/v2/push/send', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Accept-encoding': 'gzip, deflate',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(message),
+//   });
+// }
 
 
-function handleRegistrationError(errorMessage) {
-  alert(errorMessage);
-  throw new Error(errorMessage);
-}
+// function handleRegistrationError(errorMessage) {
+//   alert(errorMessage);
+//   throw new Error(errorMessage);
+// }
 
-async function registerForPushNotificationsAsync() {
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
+// async function registerForPushNotificationsAsync() {
+//   if (Platform.OS === 'android') {
+//     Notifications.setNotificationChannelAsync('default', {
+//       name: 'default',
+//       importance: Notifications.AndroidImportance.MAX,
+//       vibrationPattern: [0, 250, 250, 250],
+//       lightColor: '#FF231F7C',
+//     });
+//   }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      handleRegistrationError('Permission not granted to get push token for push notification!');
-      return;
-    }
-    const projectId =
-      Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-    if (!projectId) {
-      handleRegistrationError('Project ID not found');
-    }
-    try {
-      const pushTokenString = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-      console.log(pushTokenString);
-      return pushTokenString;
-    } catch (e) {
-      handleRegistrationError(`${e}`);
-    }
-  } else {
-    handleRegistrationError('Must use physical device for push notifications');
-  }
-}
+//   if (Device.isDevice) {
+//     const { status: existingStatus } = await Notifications.getPermissionsAsync();
+//     let finalStatus = existingStatus;
+//     if (existingStatus !== 'granted') {
+//       const { status } = await Notifications.requestPermissionsAsync();
+//       finalStatus = status;
+//     }
+//     if (finalStatus !== 'granted') {
+//       handleRegistrationError('Permission not granted to get push token for push notification!');
+//       return;
+//     }
+//     const projectId =
+//       Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+//     if (!projectId) {
+//       handleRegistrationError('Project ID not found');
+//     }
+//     try {
+//       const pushTokenString = (
+//         await Notifications.getExpoPushTokenAsync({
+//           projectId,
+//         })
+//       ).data;
+//       console.log(pushTokenString);
+//       return pushTokenString;
+//     } catch (e) {
+//       handleRegistrationError(`${e}`);
+//     }
+//   } else {
+//     handleRegistrationError('Must use physical device for push notifications');
+//   }
+// }
 
-export default function App() {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(
-    undefined
-  );
-  const notificationListener = useRef();
-  const responseListener = useRef();
+// export default function App() {
+//   const [expoPushToken, setExpoPushToken] = useState('');
+//   const [notification, setNotification] = useState(
+//     undefined
+//   );
+//   const notificationListener = useRef();
+//   const responseListener = useRef();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then(token => setExpoPushToken(token ?? ''))
-      .catch((error) => setExpoPushToken(`${error}`));
+//   useEffect(() => {
+//     registerForPushNotificationsAsync()
+//       .then(token => setExpoPushToken(token ?? ''))
+//       .catch((error) => setExpoPushToken(`${error}`));
 
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
+//     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+//       setNotification(notification);
+//     });
 
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
+//     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+//       console.log(response);
+//     });
 
-    return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(notificationListener.current);
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+//     return () => {
+//       notificationListener.current &&
+//         Notifications.removeNotificationSubscription(notificationListener.current);
+//       responseListener.current &&
+//         Notifications.removeNotificationSubscription(responseListener.current);
+//     };
+//   }, []);
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
-      <Text>Your Expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to Send Notification"
-        onPress={async () => {
-          await sendPushNotification(expoPushToken);
-        }}
-      />
-    </View>
-  );
-}
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+//       <Text>Your Expo push token: {expoPushToken}</Text>
+//       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Title: {notification && notification.request.content.title} </Text>
+//         <Text>Body: {notification && notification.request.content.body}</Text>
+//         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+//       </View>
+//       <Button
+//         title="Press to Send Notification"
+//         onPress={async () => {
+//           await sendPushNotification(expoPushToken);
+//         }}
+//       />
+//     </View>
+//   );
+// }
 
